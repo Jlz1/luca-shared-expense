7d27c3b8f6484e867173c258ffd694ea64aa7177
StevenKukilo <stevenkukilo@gmail.com>
2026-01-20 08:10:13 +0700

    add comment

--- app/src/main/java/com/example/luca/ui/Components.kt
+++ app/src/main/java/com/example/luca/ui/Components.kt
@@ -723,7 +723,7 @@ fun UserProfileOverlay(
             Box(modifier = Modifier.fillMaxWidth()) {
                 // Close Button
                 IconButton(
-                    onClick = onClose,
+                    onClick = onClose,  // TODO: logic close button
                     modifier = Modifier.align(Alignment.TopStart)
                 ) {
                     Icon(
@@ -743,7 +743,7 @@ fun UserProfileOverlay(
                         .background(UIGrey)
                         .align(Alignment.Center)
                         .clickable {
-                            // Membuka dialog pemilihan avatar
+                            // TODO: Logic Membuka dialog pemilihan avatar
                             showAvatarDialog = true
                         }
                 ) {
@@ -768,7 +768,7 @@ fun UserProfileOverlay(
 
                 // Check Button
                 IconButton(
-                    onClick = onAddContact,
+                    onClick = onAddContact, //Todo: logic add contact
                     modifier = Modifier.align(Alignment.TopEnd)
                 ) {
                     Icon(
@@ -818,7 +818,7 @@ fun UserProfileOverlay(
                     .size(56.dp)
                     .clip(CircleShape)
                     .background(UIAccentYellow)
-                    .clickable { showBankDialog = true }
+                    .clickable { showBankDialog = true }  // TODO: logic show list bank
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
